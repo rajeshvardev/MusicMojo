97c1f6204aa0079aaad60fa8aeb8ca2cc738a486
rajeshSukumaran@ForNationWide <rajesh.sukumaran@photoninfotech.net>
2016-10-13 10:22:52 -0400

    framework added

---
+++ iTunesSearch/iTunesSearch.h
@@ -0,0 +1,19 @@
+//
+//  iTunesSearch.h
+//  iTunesSearch
+//
+//  Created by RAJESH SUKUMARAN on 10/13/16.
+//  Copyright © 2016 RAJESH SUKUMARAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iTunesSearch.
+FOUNDATION_EXPORT double iTunesSearchVersionNumber;
+
+//! Project version string for iTunesSearch.
+FOUNDATION_EXPORT const unsigned char iTunesSearchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iTunesSearch/PublicHeader.h>
+
+
